3609acbc1a84ce1f34ad26ed53416a8ee2ae8b81
Adam Momen <adammuman81@gmail.com>
2023-02-19 14:04:11 +0300

    Refactor: Day 1 Rust solution to use structs

--- 2022/rust/src/day1.rs
+++ 2022/rust/src/day1.rs
@@ -1,31 +1,46 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader, Result};
+use std::path::PathBuf;
 
-pub fn solution() -> Result<i32> {
-    let file = File::open("input.txt")?;
-    let reader = BufReader::new(file);
+pub struct Solution;
 
-    let mut running_sum: i32 = 0;
-    let mut result: Vec<i32> = Vec::new();
+impl Solution {
 
-    for line in reader.lines() {
-        let cloned_line = line?.clone();
-        let is_empty = cloned_line.to_string().len() == 0;
-
-        if is_empty {
-            result.push(running_sum);
-            running_sum = 0;
-            continue;
+    fn read_file_lines(&self, file_path: &PathBuf) -> Result<Vec<String>> {
+        let file = File::open(file_path)?;
+        let reader = BufReader::new(file);
+        let mut lines = vec![];
+        for line in reader.lines() {
+            lines.push(line?);
         }
-
-        let number = cloned_line.clone().parse::<i32>().unwrap();
-        running_sum += number;
+        Ok(lines)
     }
+    pub fn solve(&self) -> Result<u32> {
+        let mut running_sum: u32 = 0;
+        let mut result: Vec<u32> = Vec::new();
 
-    result.sort();
+        let mut file_path = PathBuf::new();
+        file_path.push("/Users/adam/personal/advent_of_code/2022/input/day1.txt");
+        print!("path: {}", file_path.to_str().unwrap());
+        let lines = self.read_file_lines(&file_path).expect("Error reading the file");
+        for line in lines {
+            let cloned_line = line.clone();
+            let is_empty = cloned_line.to_string().len() == 0;
 
-    let final_result = &result[result.len() - 3..result.len()]
-        .into_iter()
-        .sum();
-    Ok(*final_result)
+            if is_empty {
+                result.push(running_sum);
+                running_sum = 0;
+                continue;
+            }
+
+            let number = cloned_line.clone().parse::<u32>().unwrap();
+            running_sum += number;
+        }
+        result.sort();
+
+        let final_result = &result[result.len() - 3..result.len()]
+            .into_iter()
+            .sum();
+        Ok(*final_result)
+    }
 }
--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -1,10 +1,11 @@
-use std::env;
 mod day1;
-use std::io::{Result};
+use std::env;
+use crate::day1::Solution;
+use std::io::Result;
 
 fn main() -> Result<()> {
     env::set_var("RUST_BACKTRACE", "1");
-    let result = day1::solution()?;
+    let result = Solution.solve().unwrap();
 
     println!("Solution is: {:?}", result);
     Ok(())
